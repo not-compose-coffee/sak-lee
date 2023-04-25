311e80c42da159f79d791ba94fe1c0c1ac597e43
issac <lsls4868@gmail.com>
2023-04-26 00:08:22 +0900

    feat : Compose Quadrant

--- week1/practice/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ week1/practice/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -17,20 +15,51 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            ComposeQuadrantApp()
+        }
     }
 }
 
 @Composable
-fun ComposeQuadrantApp() {
-    Column() {
-        Row() { }
-        Row() { }
+fun ComposeQuadrantApp(
+) {
+    Column(Modifier.fillMaxWidth()) {
+        //weight 속성은 쉽게 생각하면 비율 만약 첫 Row를 2f로 설정한다면 2비율만큼 길어짐
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(id = R.string.first_title),
+                description =stringResource(id = R.string.first_description),
+                backgroundColor = Color.Green,
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(id = R.string.second_title),
+                description =stringResource(id = R.string.second_description),
+                backgroundColor = Color.Cyan,
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(id = R.string.third_title),
+                description =stringResource(id = R.string.third_description),
+                backgroundColor = Color.DarkGray,
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(id = R.string.fourth_title),
+                description =stringResource(id = R.string.fourth_description),
+                backgroundColor = Color.Blue,
+                modifier = Modifier.weight(1f)
+            )
+        }
     }
 }
 
@@ -41,10 +70,32 @@ private fun ComposableInfoCard(
     backgroundColor: Color,
     modifier: Modifier = Modifier
 ) {
-    Column( ) { }
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = description,
+            fontSize = 16.sp,
+            //Text 간 빈 값이 왜생기나 했는데 이속성을 사용하여 줄을 가득 채우면서 빈공간이 생김
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+        ComposeQuadrantApp()
+}
