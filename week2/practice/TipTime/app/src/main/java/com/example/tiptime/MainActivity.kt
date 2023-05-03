f631c867c2fde185c28f8ecec01d3622b6cc59fa
issac <lsls4868@gmail.com>
2023-05-03 12:18:19 +0900

    feat : 키보드 버튼 설정

--- week2/practice/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ week2/practice/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,10 +60,21 @@ fun TipTImeScreen() {
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Spacer(modifier = Modifier.height(16.dp))
-        EditNumberField(R.string.bill_amount,value = amountInput, onValueChange = { amountInput = it })
+        EditNumberField(
+            R.string.bill_amount,
+            value = amountInput,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            onValueChange = { amountInput = it })
         EditNumberField(
             label = R.string.how_was_the_service,
             value = tipInput,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
             onValueChange = { tipInput = it }
         )
         Spacer(modifier = Modifier.height(24.dp))
@@ -78,8 +90,9 @@ fun TipTImeScreen() {
 
 @Composable
 fun EditNumberField(
-    @StringRes label : Int,
+    @StringRes label: Int,
     value: String,
+    keyboardOptions: KeyboardOptions,
     onValueChange: (String) -> Unit
 ) {
     TextField(
@@ -88,11 +101,11 @@ fun EditNumberField(
         label = { Text(text = stringResource(id = label)) },
         singleLine = true,
         modifier = Modifier.fillMaxWidth(),
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        keyboardOptions = keyboardOptions,
     )
 }
 
-private fun calculateTip(amount: Double, tipPercent: Double ): String {
+private fun calculateTip(amount: Double, tipPercent: Double): String {
     val tip = tipPercent / 100 * amount
     return NumberFormat.getCurrencyInstance().format(tip)
 }
