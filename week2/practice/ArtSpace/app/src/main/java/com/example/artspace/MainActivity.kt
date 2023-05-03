aca2b564038c3e5f86fb6a49b8cc9948c79c1be7
issac <lsls4868@gmail.com>
2023-05-03 16:07:24 +0900

    feat : ArtSpace Button Event

--- week2/practice/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ week2/practice/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -8,16 +8,18 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.artspace.data.ArtSpaceData
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,10 +28,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             ArtSpaceTheme {
                 // A surface container using the 'background' color from the theme {
-                    ArtSpaceApp(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentSize(Alignment.Center))
+                ArtSpaceApp(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentSize(Alignment.Center)
+                )
             }
         }
     }
@@ -38,44 +41,68 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpaceApp(modifier: Modifier = Modifier) {
+
+    var result by remember { mutableStateOf(1) }
+
+    var artSpaceData: ArtSpaceData = when (result) {
+        1 -> ArtSpaceData(R.drawable.first_art, R.string.first_art_title, R.string.first_art_author)
+
+        2 -> ArtSpaceData(
+            R.drawable.second_art,
+            R.string.second_art_title,
+            R.string.second_art_author
+        )
+
+        3 -> ArtSpaceData(R.drawable.third_art, R.string.third_art_title, R.string.third_art_author)
+
+        4 -> ArtSpaceData(
+            R.drawable.fourth_art,
+            R.string.fourth_art_title,
+            R.string.fourth_art_author
+        )
+        else -> ArtSpaceData(
+            R.drawable.first_art,
+            R.string.first_art_title,
+            R.string.first_art_author
+        )
+    }
+
+
     Column(
         modifier = modifier
             .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        ArtSpaceImage()
+        ArtSpaceImage(modifier, artSpaceData)
         Spacer(modifier = Modifier.height(16.dp))
-        ArtSpaceInfo()
+        ArtSpaceInfo(artSpaceData)
         Spacer(modifier = Modifier.height(16.dp))
-        ArtSpaceButton(modifier)
-    }
-}
+        Row(modifier = modifier, horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+            Button(onClick = {
+                if (result != 1) result -= 1
+            }) {
+                Text(text = "Previous")
+            }
 
-@Composable
-private fun ArtSpaceButton(modifier: Modifier) {
-    Row(modifier = modifier) {
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Previous")
-        }
-
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Next")
+            Button(onClick = { if (result in 1..3) result += 1 else result -= 3 }) {
+                Text(text = "Next")
+            }
         }
     }
 }
 
 @Composable
-private fun ArtSpaceInfo() {
+private fun ArtSpaceInfo(artSpaceData: ArtSpaceData) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Name",
+            text = stringResource(id = artSpaceData.description),
             fontWeight = FontWeight.Bold,
             fontSize = 32.sp
         )
         Text(
-            text = "author",
+            text = stringResource(id = artSpaceData.author),
             fontSize = 16.sp,
             fontWeight = FontWeight.Bold
         )
@@ -86,16 +113,19 @@ private fun ArtSpaceInfo() {
 @Composable
 fun ArtSpaceImage(
     modifier: Modifier = Modifier,
+    artSpaceData: ArtSpaceData
 ) {
-    Box(  modifier = Modifier
-        .fillMaxWidth()
-        .padding(16.dp)
-        .border(1.dp, color = Color.Black),) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .border(1.dp, color = Color.Black),
+    ) {
         Image(
-            modifier= modifier
+            modifier = modifier
                 .fillMaxWidth()
                 .padding(16.dp),
-            painter = painterResource(id = R.drawable.ic_launcher_background),
+            painter = painterResource(id = artSpaceData.image),
             contentDescription = "test",
             contentScale = ContentScale.FillWidth
         )
--- week2/practice/ArtSpace/app/src/main/java/com/example/artspace/data/ArtSpaceData.kt
+++ week2/practice/ArtSpace/app/src/main/java/com/example/artspace/data/ArtSpaceData.kt
@@ -2,5 +2,6 @@ package com.example.artspace.data
 
 data class ArtSpaceData(
     val image: Int,
-    val description: Int
+    val description: Int,
+    val author:Int
 )
